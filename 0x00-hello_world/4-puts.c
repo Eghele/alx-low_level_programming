2a5c73c6108a2f7436283855b6411199cb64b837
Eghele <eghele14@gmail.com>
2022-09-08 04:35:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-  int main(void)
+ *  * main - main block
+ *   * Return: 0
+ *    */
+int main(void)
 {
-  puts(“\”Programming is like building a multilingual Puzzle”);
-  return (0);
- }
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
