481c5412a4eb79370e4e9aace3516c97fd9c9d94
Eghele <eghele14@gmail.com>
2022-09-08 06:21:39 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - main function in the task
+ *
+ *
+ *Return: return 1
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
