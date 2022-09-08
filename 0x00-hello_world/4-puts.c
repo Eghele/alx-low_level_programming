0d88d081726f9ed2c8bfd85a32fd6ab7bb385ae3
Eghele <eghele14@gmail.com>
2022-09-08 04:04:58 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+ /**
+  * main - content of puts
+  *
+  * Return: 0 after printing the function (success)
+ */
+  int main(void)
+  (puts(“\”Programming is like building a multi ilingual Puzzle”)
+          return (0);
+  )
