155487b556df7b414ba8d427a0919361c2330a9e
Eghele <eghele14@gmail.com>
2022-09-09 12:03:03 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (j == 57 && i == 56)
+				{
+				
+					break
+						;
+				}
+				
+				putchar(',');
+				putchar(' ');
+			
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
