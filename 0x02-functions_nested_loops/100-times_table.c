0106848c22d10ac59551b2b6f7e868c15da1e20d
Eghele <eghele14@gmail.com>
2022-09-13 18:20:34 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,6 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
-
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
@@ -28,7 +27,6 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
-
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
@@ -36,7 +34,6 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
-
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
