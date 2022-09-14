5ba93e922d1094723dada9ddafb4e5db40d7abf8
Eghele <eghele14@gmail.com>
2022-09-13 17:09:39 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,13 +13,17 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-											} else if (n == 0)
-											{
+
+	} else if (n == 0)
+
+	{
 		_putchar(48);
 		return (0);
-											} else if (n < 0)
-											{
-												_putchar('-');
-											}
-												return (-1);
+
+	} else if (n < 0)
+
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
