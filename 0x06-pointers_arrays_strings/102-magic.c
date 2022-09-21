d619bbebb9ffa149462e88f0b2a9043c076cee9f
Eghele <eghele14@gmail.com>
2022-09-21 13:22:41 -0700

    10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+
 	int n;
 	int a[5];
 	int *p;
@@ -19,5 +20,7 @@ int main(void)
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
+
 }
