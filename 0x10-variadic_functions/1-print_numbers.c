4095c538b107c1f729accffe06c1f50b20a7b206
Eghele <eghele14@gmail.com>
2022-10-13 08:57:49 -0700

    checked

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,26 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
- * print_numbers - prints num
- * @separator: pointer param
- * @n: param
+ * print_numbers - print variadic number
+ * @separator: char
+ * @n:variadic variable count
+ * Return :void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list list;
 	unsigned int i;
 
-	va_start(valist, n);
-
+	va_start(list, n);
 	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator && i < n - 1)
 		{
-			printf("%d", va_arg(valist, int));
-			if (separator && i < n - 1)
-				printf("%s", separator);
+			printf("%s", separator);
 		}
-
-		printf("\n");
-		va_end(valist);
+	}
+	printf("\n");
+	va_end(list);
 }
